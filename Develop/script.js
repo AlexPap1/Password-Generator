5e08888aec3384301ff1f07f016234913de873c9
AlexPap <arpappagallo@gmail.com>
2022-03-24 23:10:44 -0400

    start challenge 3. Strings and functuions done!

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,20 @@
 // Assignment code here
+window.alert("Time to generate a new passoword!");
+
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var number = "1234567890";
+var special = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+//see \ yellow font
+var allchar = "";
+
+var lowercase = window.confirm("Lower Case?")
+  if (lowercase) {
+    
+  }
+  else {
+
+  }
 
 
 // Get references to the #generate element
@@ -6,6 +22,21 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  window.prompt("How many characters would you like?")
+    if () {
+      window.alert("Please enter a value between 8 and 128.")
+    }
+    else {
+      confirmNumber = window.confirm("Would you like numbers in your password?")
+        if
+      confirmLowercase = window.confirm ("Would you like lowercase letters in your password?")
+        if
+      confirmUppercase = window.confirm ("Would you like uppercase letters in your password?")
+        if
+      confirmSpecial = window.confirm ("Would you like special characters in your password?")
+        if
+      confirmAllchar = window.confirm 
+    }
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -15,3 +46,12 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Start function to generate password
+function generatePassword() {
+  // Asks for user input
+  window.prompt("How many characters ar erequired?")
+    if { 
+      
+    }
+}
